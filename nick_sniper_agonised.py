45933a376bcdde58027112bf660015e5c4ef54e6
Kyso <128049220+kyso1@users.noreply.github.com>
2023-08-14 08:48:19 -0300

    nick_sniper_agonised_v1.1.py

--- nick_sniper_agonised.py
+++ nick_sniper_agonised.py
@@ -23,21 +23,26 @@ async def connect(connection):
                 #if the name is found, you can change your name with this code:
                 if changename == True:
                     # Check for Essences and RP
-                    essences_response = await connection.request('get', '/lol-store/v1/wallet/purchase-credits')
+                    essences_response = await connection.request('get', '/lol-inventory/v1/wallet/purchase-credits')                        
                     essences_data = await essences_response.json()
-                    blue_essences = essences_data.get('balance')
-                    rp_response = await connection.request('get', '/lol-store/v1/wallet/rp')
-                    rp_data = await rp_response.json()
-                    rp_balance = rp_data.get('rp')
-                    try: 
+                    blue_essences = essences_data.get('lol_blue_essence')
+                    riot_points = essences_data.get('RP')
+                    if(blue_essences>=13900):     
                         print("Nick found and conditions met (RP or BE sufficients) ! Attempting to change the nick to:", nick)
-                        response = await connection.request('post', '/lol-summoner/v1/current-summoner/name', data=json.dumps({"name": nick}), headers=headers)
-                        print("Nick changed successfully!")
+                        response = await connection.request('put', '/lol-summoner/v1/current-summoner/name', data=json.dumps({"name": nick}))
+                        print("Nick changed successfully! 13900 BE Spent")
                         print("\n")
                         input("Press Enter to end the program...")
                         exit(0)
-                    except:
-                        print("Conditions not met to change the name. RP or BE missing.")
+                    elif(riot_points>=1300):
+                        print("Nick found and conditions met (RP or BE sufficients) ! Attempting to change the nick to:", nick)
+                        response = await connection.request('put', '/lol-summoner/v1/current-summoner/name', data=json.dumps({"name": nick}))
+                        print("Nick changed successfully! 1300 RP Spent")
+                        print("\n")
+                        input("Press Enter to end the program...")
+                        exit(0)
+                    else:      
+                        print("Conditions not met to change the name. Missing BE, make sure you have 13900 BE to change the nick.")
                         print("Change your account and try again.")
                         print("\n")
                         input("Press Enter to end the program...")
